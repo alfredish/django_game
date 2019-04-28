3411f35990a09185baf0a60f456643c6502516bb
alfredish <kira_001@mail.ru>
2019-04-28 10:26:49 +0300

    шаг вперед
    Добавил форму и вывод форму корявый

--- game/qwerty/views.py
+++ game/qwerty/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
 import requests
+from bs4 import BeautifulSoup
+from .forms import Form
+
 
 def index(request):
-    return render(request,'index.html')
+'''Главная функция'''
+    url = "https://translate.yandex.ru/?lang=ru-en&text={}"
+    r = requests.get(url)
+    var = BeautifulSoup(r.text)
+
+
+    if(request.method == 'POST'):
+        form = Form(request.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+
+            context = {
+                'var': var,
+                'form': form,
+                'name': name,
+            }
+
+            return render(request,'index.html',context)
+    else:
+        form = Form()
+
+        context = {
+            "var":var,
+            'form':form,
+        }
+
+        return render(request,'index.html',context)
